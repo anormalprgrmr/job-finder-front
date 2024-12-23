0bfb35c6dc9e3fcfaa6750cb5b0dd6fbd36129a1
Abolfazl Ammarloo <128920709+anormalprgrmr@users.noreply.github.com>
2024-12-23 17:31:21 +0330

    login and sign up components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   {
     path: 'auth',
     component: AuthComponent,
