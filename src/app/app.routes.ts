ef15e58d7b60fc8fc24f4a7f34fb1e7bcdd7cbc6
Abolfazl Ammarloo <128920709+anormalprgrmr@users.noreply.github.com>
2024-12-23 16:14:52 +0330

    home page component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [{ path: 'login', component: LoginComponent }],
+  },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,ts}" // Ensure it scans all Angular templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
