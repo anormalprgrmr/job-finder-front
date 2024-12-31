e0535d9ae175bf2dbfedc742946cdecdda63ecbe
anormalprgrmr <abolfazl.amm.hcb@gmail.com>
2024-12-31 18:25:36 +0330

    company modals fixed

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-company',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './company.component.html',
   styleUrl: './company.component.css'
 })
 export class CompanyComponent {
+  appModal:boolean = false ;
+  reqModal:boolean = false ;
+  
+  toggleAppModal(){
+    this.appModal = !this.appModal;
+  }
+
+  toggleReq(){
+    console.log('heyy');
+    
+    this.reqModal = !this.reqModal;
+  }
 
 }
